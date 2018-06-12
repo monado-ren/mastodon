d17dd54d620fd58ef8eaaa03dc34dfaafec5880b
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-06-12 08:52:29 +0200

    Point source to masto-donte-com-br instead

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -25,7 +25,7 @@ module Mastodon
     end
 
     def build_metadata
-      ENV.fetch('MASTODON_VERSION_METADATA', nil)
+      ENV.fetch('MASTODON_VERSION_METADATA', 'donte')
     end
 
     def to_a
@@ -44,7 +44,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'masto-donte-com-br/mastodon')
     end
 
     def source_base_url
