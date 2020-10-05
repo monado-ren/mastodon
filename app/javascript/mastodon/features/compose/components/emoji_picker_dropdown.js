faa2cffae0e351c56f6d066316074ed9a3981fb6
chasedream1129 <36710631+chasedream1129@users.noreply.github.com>
2020-10-05 15:49:04 +0800

    enable emoji preview

--- app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
+++ app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
@@ -251,10 +251,10 @@ class EmojiPickerMenu extends React.PureComponent {
         <EmojiPicker
           perLine={8}
           emojiSize={22}
-          sheetSize={32}
+          sheetSize={64}
           custom={buildCustomEmojis(custom_emojis)}
           color=''
-          emoji=''
+          emoji='grinning'
           set='twitter'
           title={title}
           i18n={this.getI18n()}
@@ -262,7 +262,7 @@ class EmojiPickerMenu extends React.PureComponent {
           include={categoriesSort}
           recent={frequentlyUsedEmojis}
           skin={skinTone}
-          showPreview={false}
+          showPreview
           backgroundImageFn={backgroundImageFn}
           autoFocus
           emojiTooltip
