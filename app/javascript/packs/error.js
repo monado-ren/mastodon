3ef983bdc824c53bf26db4b53ab3390adc9217a6
Monado Toolman <master@mona.do>
2024-08-12 21:01:07 +0800

    修改oops图片为CD子&注释鼠标悬停事件

--- app/javascript/packs/error.js
+++ app/javascript/packs/error.js
@@ -4,11 +4,11 @@ import ready from '../mastodon/ready';
 ready(() => {
   const image = document.querySelector('img');
 
-  image.addEventListener('mouseenter', () => {
-    image.src = '/oops.gif';
-  });
+//  image.addEventListener('mouseenter', () => {
+//    image.src = '/oops.gif';
+//  });
 
-  image.addEventListener('mouseleave', () => {
-    image.src = '/oops.png';
-  });
+//  image.addEventListener('mouseleave', () => {
+//    image.src = '/oops.png';
+//  });
 });
